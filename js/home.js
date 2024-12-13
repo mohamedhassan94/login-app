b016267caa60f430c1984067f4fa9e3863f675f5
Mohamed Hassan <mo.ref.has94@gmail.com>
2024-12-13 18:44:22 +0200

    Fix absolute paths

--- js/home.js
+++ js/home.js
@@ -21,7 +21,7 @@ function displayWelcomeMessage() {
     welcomeMessage.textContent = `Welcome, ${user.name}!`;
   } else {
     alert("You are not logged in. Redirecting to login page.");
-    window.location.href = "/login-app/";
+    window.location.replace("index.html");
   }
 }
 
@@ -34,10 +34,10 @@ function handleSignOut(e) {
     users[userIndex].isLogin = false;
     saveUsers(users);
     alert("You have been signed out successfully.");
-    window.location.href = "/login-app/";
+    window.location.replace("index.html");
   } else {
     alert("No user is currently logged in.");
-    window.location.href = "/login-app/";
+    window.location.replace("index.html");
   }
 }
 
